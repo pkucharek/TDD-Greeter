55ac453b8c07da6f87b47476709c82a2dd68fe80
Piotr Kucharek <piotrkucharek1@gmail.com>
2021-05-14 10:32:26 +0200

    (BLUE) Refactored Greeter

--- src/main/java/com/pkucharek/Greeter.java
+++ src/main/java/com/pkucharek/Greeter.java
@@ -17,19 +17,27 @@ public class Greeter {
     }
 
     public String greet(String name) {
-        String trimmed = name.trim();
-        String capitalized = StringUtils.capitalize(trimmed);
+        String beginning = getGreetingBeginning();
+        String correctedName = getCorrectedName(name);
 
-        String greeting;
+        return String.format("%s %s", beginning, correctedName);
+    }
 
+    private String getGreetingBeginning() {
+        if (isMorning())
+            return "Good morning";
+        return "Hello";
+    }
+
+    private boolean isMorning() {
         LocalTime actualTime = timeProvider.provide();
-        if (actualTime.isAfter(LocalTime.of(5, 59, 59))
-                && actualTime.isBefore(LocalTime.of(12, 0, 1)))
-            greeting = "Good morning";
-        else
-            greeting = "Hello";
+        return actualTime.isAfter(LocalTime.parse("05:59:59"))
+                && actualTime.isBefore(LocalTime.parse("12:00:01"));
+    }
 
-        return String.format("%s %s", greeting, capitalized);
+    private String getCorrectedName(String name) {
+        String trimmedName = name.trim();
+        return StringUtils.capitalize(trimmedName);
     }
 
 }
