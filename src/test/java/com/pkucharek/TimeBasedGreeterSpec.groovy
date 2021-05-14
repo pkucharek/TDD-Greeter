c201c7651ee0ee6790abd7727be2b0cf0a96ecd0
Piotr Kucharek <piotrkucharek1@gmail.com>
2021-05-14 11:00:03 +0200

    (RED) extracted tests per each time range
    In addition changed time range to be left hand half-open interval.

--- src/test/java/com/pkucharek/TimeBasedGreeterSpec.groovy
+++ src/test/java/com/pkucharek/TimeBasedGreeterSpec.groovy
@@ -6,43 +6,48 @@ import java.time.LocalTime
 
 class TimeBasedGreeterSpec extends Specification {
 
-    def "returns Good morning when time is between 06:00 and 12:00"() {
+    def "returns Good morning when time is in (06:00, 12:00]"() {
         expect:
             timeBasedGreeter(time).greet("John") == result
 
         where:
             time       | result
-            "05:59:59" | "Hello John"
-            "06:00:00" | "Good morning John"
+            "06:00:01" | "Good morning John"
             "08:00:00" | "Good morning John"
             "12:00:00" | "Good morning John"
-            "12:00:01" | "Hello John"
     }
 
-    def "returns Good evening when time is between 18:00 and 22:00"() {
+    def "returns Hello when time is in (12:00, 18:00]"() {
         expect:
             timeBasedGreeter(time).greet("John") == result
 
         where:
             time       | result
-            "17:59:59" | "Hello John"
-            "18:00:00" | "Good evening John"
+            "12:00:01" | "Hello John"
+            "15:00:00" | "Hello John"
+            "18:00:00" | "Hello John"
+    }
+
+    def "returns Good evening when time is in (18:00, 22:00]"() {
+        expect:
+            timeBasedGreeter(time).greet("John") == result
+
+        where:
+            time       | result
+            "18:00:01" | "Good evening John"
             "20:00:00" | "Good evening John"
             "22:00:00" | "Good evening John"
-            "22:00:01" | "Hello John"
     }
 
-    def "returns Good night when time is between 22:00:01 and 06:00"() {
+    def "returns Good night when time is in (22:00, 06:00]"() {
         expect:
             timeBasedGreeter(time).greet("John") == result
 
         where:
             time       | result
-            "22:00:00" | "Good evening John"
             "22:00:01" | "Good night John"
             "03:00:00" | "Good night John"
             "06:00:00" | "Good night John"
-            "06:00:01" | "Good morning John"
     }
 
     def timeBasedGreeter(String time) {
