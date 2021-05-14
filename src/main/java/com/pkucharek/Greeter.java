1d983dd32232a34e2078990d5be9d24b956d6434
Piotr Kucharek <piotrkucharek1@gmail.com>
2021-05-14 09:42:08 +0200

    Input name is trimmed

--- src/main/java/com/pkucharek/Greeter.java
+++ src/main/java/com/pkucharek/Greeter.java
@@ -3,7 +3,7 @@ package com.pkucharek;
 public class Greeter {
 
     public String greet(String name) {
-        return String.format("Hello %s", name);
+        return String.format("Hello %s", name.trim());
     }
 
 }
