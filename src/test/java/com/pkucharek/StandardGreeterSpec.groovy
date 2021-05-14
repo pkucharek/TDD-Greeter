1986191a081ad078a7dd504d559cae663f644d0a
Piotr Kucharek <piotrkucharek1@gmail.com>
2021-05-14 10:23:39 +0200

    (BLUE) Extracted TimeBased tests

---
+++ src/test/java/com/pkucharek/StandardGreeterSpec.groovy
@@ -0,0 +1,41 @@
+package com.pkucharek
+
+import spock.lang.Specification
+
+import java.time.LocalTime
+
+class StandardGreeterSpec extends Specification {
+
+    Greeter standardGreeter = new Greeter(() -> LocalTime.parse("05:00:00"))
+
+    def "returns Hello #name"() {
+        expect:
+            standardGreeter.greet(name) == result
+
+        where:
+            name   | result
+            "Jack" | "Hello Jack"
+            "Bob"  | "Hello Bob"
+    }
+
+    def "input name is trimmed"() {
+        expect:
+            standardGreeter.greet(name) == result
+
+        where:
+            name         | result
+            "  James  "  | "Hello James"
+            "\nEvelyn\t" | "Hello Evelyn"
+    }
+
+    def "input name #name is capitalized"() {
+        expect:
+            standardGreeter.greet(name) == result
+
+        where:
+            name     | result
+            "harper" | "Hello Harper"
+            "mason"  | "Hello Mason"
+    }
+
+}
