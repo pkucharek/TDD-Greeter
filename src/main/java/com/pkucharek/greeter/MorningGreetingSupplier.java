9befac0719694e6bea931944f4c745f236b93295
Piotr Kucharek <piotrkucharek1@gmail.com>
2021-05-18 14:41:01 +0200

    Added final fields for times so they will be initialized only once

--- src/main/java/com/pkucharek/greeter/MorningGreetingSupplier.java
+++ src/main/java/com/pkucharek/greeter/MorningGreetingSupplier.java
@@ -3,10 +3,12 @@ package com.pkucharek.greeter;
 import java.time.LocalTime;
 
 class MorningGreetingSupplier implements TimeBasedGreetingSupplier {
+    private final LocalTime TIME_06_00_00 = LocalTime.parse("06:00:00");
+    private final LocalTime TIME_12_00_01 = LocalTime.parse("12:00:01");
+
     @Override
     public boolean test(LocalTime time) {
-        return time.isAfter(LocalTime.parse("06:00:00"))
-                && time.isBefore(LocalTime.parse("12:00:01"));
+        return time.isAfter(TIME_06_00_00) && time.isBefore(TIME_12_00_01);
     }
 
     @Override
--- src/main/java/com/pkucharek/greeter/NightGreetingSupplier.java
+++ src/main/java/com/pkucharek/greeter/NightGreetingSupplier.java
@@ -3,10 +3,12 @@ package com.pkucharek.greeter;
 import java.time.LocalTime;
 
 class NightGreetingSupplier implements TimeBasedGreetingSupplier {
+    private final LocalTime TIME_22_00_00 = LocalTime.parse("22:00:00");
+    private final LocalTime TIME_06_00_01 = LocalTime.parse("06:00:01");
+
     @Override
     public boolean test(LocalTime time) {
-        return time.isAfter(LocalTime.parse("22:00:00"))
-            || time.isBefore(LocalTime.parse("06:00:01"));
+        return time.isAfter(TIME_22_00_00) || time.isBefore(TIME_06_00_01);
     }
 
     @Override
